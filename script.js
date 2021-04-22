77b6d968226a7d09a746452260ba3094ca2fc05e
Skandar Swayah <skandarswayah0@gmail.com>
2021-04-22 14:57:50 +0100

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let btnPlus = document.getElementsByClassName("incr")
 console.log(btnPlus)
+// let sp = document.getElementsByClassName('sp');
+// console.log(sp)
 
 for (let el of btnPlus) {
     el.addEventListener('click', function () {
         el.nextElementSibling.nextElementSibling.value++
-       um()
+        // let val = document.getElementsByClassName('inc').value
+        // console.log(val)
+        // somme(val, sp)
+        sum()
     })
 }
 
@@ -21,24 +26,33 @@ for (let el of btnMoins) {
     })
 }
 
-function sum(){
+// function somme(x, y) {
+//     console.log(x * y)
+// }
+// console.log(somme(val, sp))
+// // let val = document.getElementById('inc').value
+// //             console.log(val)
+
+// console.log(val)
+
+function sum() {
     let prices = document.getElementsByClassName('sp');
     console.log(prices[1].innerHTML)
     let qte = document.getElementsByClassName('inc');
     console.log(qte[1].value)
 
 
-    let s = 0 
+    let s = 0
     let m = 0
-    for (let i=0 ; i<prices.length;i++){
-   s+= prices[i].innerHTML * qte[i].value
-   m-=prices[i].innerHTML * qte[i].value
-  let pr=s+m
-   console.log(pr)
+    for (let i = 0; i < prices.length; i++) {
+        s += prices[i].innerHTML * qte[i].value
+        m -= prices[i].innerHTML * qte[i].value
+        let pr = s + m
+        console.log(pr)
     }
 
     return document.getElementById('t').innerHTML = s
-    
+
 
 }
 
